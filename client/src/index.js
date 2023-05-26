0f6900ad01f0049648699e36c530193934479113
fdrive422 <fafrago@gmail.com>
2023-05-25 19:28:13 -0700

    inital set up of client

---
+++ client/src/index.js
@@ -0,0 +1,23 @@
+import { configureStore } from '@reduxjs/toolkit';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import globalReducer from "state";
+import App from './App';
+import './index.css';
+
+const store = configureStore({
+  reducer: {
+    global: globalReducer,
+  }
+})
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
+
