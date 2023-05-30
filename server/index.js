fe3a9ce80fe98697ddfdd1e50f0fea4cefda1419
fdrive422 <fafrago@gmail.com>
2023-05-29 18:42:21 -0700

    setup backend connection to mongoDb

--- server/index.js
+++ server/index.js
@@ -10,6 +10,10 @@ import generalRoutes from './routes/general.js';
 import managementRoutes from './routes/management.js';
 import salesRoutes from './routes/sales.js';
 
+// Data Imports
+import User from "./models/User.js";
+import { dataUser } from "./data/index.js";
+
 
 // CONFIGURATION
 dotenv.config();
@@ -30,9 +34,14 @@ app.use("/sales", salesRoutes);
 
 // MONGOOSE SETUP
 const PORT = process.env.PORT || 9000;
-mongoose.connect(process.env.MONGO_URL, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-}).then(() => {
-	app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
-}).catch((error) => console.log(`${error} did not connect`));
+mongoose
+	.connect(process.env.MONGO_URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	}).then(() => {
+		app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+		// ONLY ADD DATA ONE TIME
+		User.insertMany(dataUser);
+
+	}).catch((error) => console.log(`${error} did not connect`));
