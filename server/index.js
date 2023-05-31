4128270b294dd7676875ecf64a40f0ba6a4025af
fdrive422 <fafrago@gmail.com>
2023-05-30 17:59:57 -0700

    map user profile to sidebar

--- server/index.js
+++ server/index.js
@@ -42,6 +42,6 @@ mongoose
 		app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
 		// ONLY ADD DATA ONE TIME
-		User.insertMany(dataUser);
+		// User.insertMany(dataUser);
 
 	}).catch((error) => console.log(`${error} did not connect`));
