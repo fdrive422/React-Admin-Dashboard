078bed618bf0bd57dd32d710811f1f0dfcf5ff11
fdrive422 <fafrago@gmail.com>
2023-06-03 20:01:00 -0700

    build transaction table

--- server/index.js
+++ server/index.js
@@ -1,26 +1,31 @@
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import express from 'express';
-import helmet from 'helmet';
-import mongoose from 'mongoose';
-import morgan from 'morgan';
-import clientRoutes from './routes/client.js';
-import generalRoutes from './routes/general.js';
-import managementRoutes from './routes/management.js';
-import salesRoutes from './routes/sales.js';
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import helmet from "helmet";
+import mongoose from "mongoose";
+import morgan from "morgan";
+import clientRoutes from "./routes/client.js";
+import generalRoutes from "./routes/general.js";
+import managementRoutes from "./routes/management.js";
+import salesRoutes from "./routes/sales.js";
 
 // Data Imports
 import User from "./models/User.js";
-import Product from "./models/Product.js"
-import ProductStat from "./models/ProductStat.js"
-import { dataUser, dataProduct, dataProductStat } from "./data/index.js";
-
+import Product from "./models/Product.js";
+import ProductStat from "./models/ProductStat.js";
+import Transaction from "./models/Transaction.js";
+import {
+	dataUser,
+	dataProduct,
+	dataProductStat,
+	dataTransaction,
+} from "./data/index.js";
 
 // CONFIGURATION
 dotenv.config();
 const app = express();
-app.use(express.json())
+app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
@@ -40,12 +45,14 @@ mongoose
 	.connect(process.env.MONGO_URL, {
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
-	}).then(() => {
+	})
+	.then(() => {
 		app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
 		// ONLY ADD DATA ONE TIME
 		// Product.insertMany(dataProduct);
 		// ProductStat.insertMany(dataProductStat);
+		// Transaction.insertMany(dataTransaction);
 		// User.insertMany(dataUser);
-
-	}).catch((error) => console.log(`${error} did not connect`));
+	})
+	.catch((error) => console.log(`${error} did not connect`));
