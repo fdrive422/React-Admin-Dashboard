ed6c26ed20a94e558dcec3e3350d300cc779723b
fdrive422 <fafrago@gmail.com>
2023-06-11 12:48:24 -0700

    create monthly sales line chart

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import Transactions from "scenes/transactions";
 import Geography from "scenes/geography";
 import Overview from "scenes/overview";
 import Daily from "scenes/daily";
+import Monthly from "scenes/monthly";
 
 // Set up dark mode
 function App() {
@@ -39,6 +40,7 @@ function App() {
 							<Route path="/geography" element={<Geography />} />
 							<Route path="/overview" element={<Overview />} />
 							<Route path="/daily" element={<Daily />} />
+							<Route path="/monthly" element={<Monthly />} />
 						</Route>
 					</Routes>
 				</ThemeProvider>
