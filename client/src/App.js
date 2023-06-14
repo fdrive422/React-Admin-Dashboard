a8003db7fc7e28551cd5bf1afd005d05bdcad157
fdrive422 <fafrago@gmail.com>
2023-06-13 19:11:43 -0700

    create breakdown pie chart

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import Geography from "scenes/geography";
 import Overview from "scenes/overview";
 import Daily from "scenes/daily";
 import Monthly from "scenes/monthly";
+import Breakdown from "scenes/breakdown";
 
 // Set up dark mode
 function App() {
@@ -41,6 +42,7 @@ function App() {
 							<Route path="/overview" element={<Overview />} />
 							<Route path="/daily" element={<Daily />} />
 							<Route path="/monthly" element={<Monthly />} />
+							<Route path="/breakdown" element={<Breakdown />} />
 						</Route>
 					</Routes>
 				</ThemeProvider>
