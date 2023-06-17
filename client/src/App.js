f28d5b3e361ec8ed6bbccc75492fcaa75809c428
fdrive422 <fafrago@gmail.com>
2023-06-16 17:49:51 -0700

    complete admin and finalize dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,7 @@ import Daily from "scenes/daily";
 import Monthly from "scenes/monthly";
 import Breakdown from "scenes/breakdown";
 import Admin from "scenes/admin";
+import Performance from "scenes/performance";
 
 // Set up dark mode
 function App() {
@@ -45,6 +46,10 @@ function App() {
 							<Route path="/monthly" element={<Monthly />} />
 							<Route path="/breakdown" element={<Breakdown />} />
 							<Route path="/admin" element={<Admin />} />
+							<Route
+								path="/performance"
+								element={<Performance />}
+							/>
 						</Route>
 					</Routes>
 				</ThemeProvider>
--- client/src/state/api.js
+++ client/src/state/api.js
@@ -11,6 +11,8 @@ export const api = createApi({
 		"Geography",
 		"Sales",
 		"Admins",
+		"Performance",
+		"Dashboard",
 	],
 	endpoints: (build) => ({
 		getUser: build.query({
@@ -45,6 +47,14 @@ export const api = createApi({
 			query: () => "management/admins",
 			providesTags: ["Admins"],
 		}),
+		getUserPerformance: build.query({
+			query: (id) => `management/performance/${id}`,
+			providesTags: ["Performance"],
+		}),
+		getDashboard: build.query({
+			query: () => "general/dashboard",
+			providesTags: ["Dashboard"],
+		}),
 	}),
 });
 
@@ -56,4 +66,6 @@ export const {
 	useGetGeographyQuery,
 	useGetSalesQuery,
 	useGetAdminsQuery,
+	useGetUserPerformanceQuery,
+	useGetDashboardQuery,
 } = api;
